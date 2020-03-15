b6aae21a4d989843262d4d83355ea24ebf31d8a4
jedlejedi <julienlafontaine2006@gmail.com>
2020-03-15 22:23:15 +1100

    Used Trigram model to improve prediction accuracy for quiz 2

--- DataPreparation.R
+++ DataPreparation.R
@@ -3,11 +3,11 @@ library(tm)
 raw_data_folder <- "/home/julien/data-science/DataScienceCapstone/data/raw/en_US"
 work_data_folder <- "/home/julien/data-science/DataScienceCapstone/data/work"
 
-create_data_file <- function(input_folder, output_file, sample = FALSE) {
+create_data_file <- function(input_folder, output_folder, sample = FALSE) {
   
   files <- list.files(raw_data_folder)
   
-  fc_output <- file(output_file)
+  fc_output <- file(paste(output_folder, "en_US.txt", sep = "/"))
   
   for (f in files) {
     fc <- file(paste(raw_data_folder, f, sep = "/"))
@@ -25,27 +25,41 @@ create_data_file <- function(input_folder, output_file, sample = FALSE) {
 }
 
 cleanPunctation <- function(x) {
-  x1 <- gsub('“', '"', x)
-  gsub("’", "'", x1)
-}
-
-removeSpecialCharacters <- function(x) {
-  x1 <- gsub("😂|⁰", " ", x)
-  gsub("♥", " ", x1)
+  x1 <- gsub('“|”', '"', x)
+  x1 <- gsub("’|‘", "'", x1)
+  x1 <- gsub("…|'", " ", x1)
+  x1 <- gsub("¿|¡", " ", x1)
+  x1 <- gsub("—|–|―", " ", x1)
+  x1 <- gsub("♥|⁰|•", " ", x1)
+  x1
 }
 
 create_corpus <- function(source_folder)  {
   c <- VCorpus(DirSource(source_folder))
   c1 <- tm_map(c, removeNumbers)
-  c1 <- tm_map(c1, content_transformer(removeSpecialCharacters))
   c1 <- tm_map(c1, content_transformer(cleanPunctation))
   c1 <- tm_map(c1, removePunctuation, ucp = FALSE)
   c1 <- tm_map(c1, stripWhitespace)
   c1
 }
 
-sample_file <- paste(work_data_folder, "en_US_sample.txt", sep = "/")
+sample_file_folder <- paste(work_data_folder, "sample", sep = "/")
+full_file_folder <- paste(work_data_folder, "full", sep = "/")
 
-create_data_file(raw_data_folder, sample_file, TRUE)
+# create_data_file(raw_data_folder, sample_file_folder, TRUE)
+# c <- create_corpus(sample_file_folder)
 
-c <- create_corpus(work_data_folder)
+#create_data_file(raw_data_folder, full_file_folder, FALSE)
+c <- create_corpus(full_file_folder)
+
+# tdm1 <- TermDocumentMatrix(c)
+# 
+# m1 <- as.matrix(tdm1)
+
+test_cleanPunctation <- function() {
+  print(cleanPunctation('ask”') == 'ask"')
+  print(cleanPunctation("¿what if i was shallow?") == " what if i was shallow?")
+  print(cleanPunctation("— but not impossible —") == "  but not impossible  ")
+  print(cleanPunctation("I'd like") == "I d like")
+  print(cleanPunctation("— but not impossible —") == "  but not impossible  ")
+}
--- PredictionModel.R
+++ PredictionModel.R
@@ -1,32 +1,66 @@
+options(java.parameters = "-Xmx2048m")
+
 library(tm)
-
-tdm1 <- TermDocumentMatrix(c)
-
-m1 <- as.matrix(tdm1)
-
-
 library(RWeka)
-
-BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
-
-tdm2 <- TermDocumentMatrix(c, control = list(tokenize = BigramTokenizer))
-
-m2 <- as.matrix(tdm2)
-
-t2 <- strsplit(rownames(m2), " ")
-
-df <- data.frame(matrix(unlist(t2), nrow=length(t2), byrow=T), unname(m2))
-
-names(df) <- c("Term1", "Term2", "Occurence")
-
+library(tidyr)
 library(dplyr)
 
-findNextTerm <- function(term1, term2) {
-  head(filter(df, Term1 == term1, Term2 == term2) %>% arrange(desc(Occurence)))  
+TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+
+tdm <- TermDocumentMatrix(c, control = list(tokenize = TrigramTokenizer))
+
+m <- as.matrix(tdm)
+rm(tdm)
+
+Terms <- rownames(m)
+Occurence <- unname(m)
+rm(m)
+
+df <- data.frame(Terms, Occurence)
+rm(Terms, Occurence)
+
+df3 <- separate(df, Terms, c("Term1","Term2","Term3"), " ")
+
+df2 <- group_by(df3, Term1, Term2) %>% summarise(Occurence = sum(Occurence)) %>% ungroup()
+
+quiz <- function(term1, term2, w1, w2, w3, w4) {
+  res <- filter(df3, 
+         Term1 == term1, 
+         Term2 == term2, 
+         Term3 == w1 | 
+           Term3 == w2 | 
+           Term3 == w3 | 
+           Term3 == w4) %>% arrange(desc(Occurence))
+  if(nrow(res) == 0) {
+    res <- filter(df2, 
+                  Term1 == term2, 
+                  Term2 == w1 | 
+                    Term2 == w2 | 
+                    Term2 == w3 | 
+                    Term2 == w4) %>% arrange(desc(Occurence))
+  }
+  head(res)
 }
 
-quiz3 <- function(term1, term2) {
-  filter(df, Term1 == term1, Term2 == term2[1] | Term2 == term2[2] | Term2 == term2[3] | Term2 == term2[4]) %>% arrange(desc(Occurence))
-}
 
-quiz3("in", c("toe", "arm", "hand", "finger"))
+quiz("case", "of", "cheese", "beer", "sode", "pretzels")
+quiz("mean", "the", "best", "most", "universe", "world")
+quiz("me","the", "saddest", "smelliest", "bluest", "happiest")
+quiz("but","the", "defense", "referees", "players", "crowd")
+quiz("at","the", "mall", "beach", "grocery", "movies")
+quiz("on","my", "phone", "horse", "way", "motorcycle")
+quiz("quite","some", "thing", "time", "weeks", "years")
+quiz("his","little", "eyes", "toes", "fingers", "ears")
+quiz("during","the", "hard", "sad", "worse", "bad")
+quiz("must","be", "asleep", "callous", "insensitive", "insane")
+# 
+# quiz("i", "d", "die", "eat", "give", "sleep")
+# quiz("about", "his", "financial", "spiritual", "marital", "horticultural")
+# quiz("monkeys","this", "weekend", "decade", "morning", "month")
+# quiz("reduce","your", "stress", "sleepiness", "happyness", "hunger")
+# quiz("take","a", "walk", "picture", "minute", "look")
+# quiz("settle","the", "case", "matter", "account", "incident")
+# quiz("in","each", "toe", "arm", "hand", "finger")
+# quiz("to","the", "side", "middle", "centre", "top")
+# quiz("from","playing", "outside", "daily", "inside", "weekly")
+# quiz("sandlers","s", "pictures", "novels", "stories", "movie")
